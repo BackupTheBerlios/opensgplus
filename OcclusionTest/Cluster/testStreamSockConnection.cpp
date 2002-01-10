628c0438b57ee51a59b0e33a6d840cddab29abf0
jsux <jsux>
2002-01-10 08:23:05 +0000

    Initial revision

---
+++ OcclusionTest/Cluster/testStreamSockConnection.cpp
@@ -0,0 +1,54 @@
+#include <OSGConfig.h>
+#include <OSGBaseFunctions.h>
+#include <OSGStreamSockConnection.h>
+
+using namespace OSG;
+
+int main(int argc,char **argv)
+{
+    UInt32 i,j;
+
+ 	// OSG init
+    osgInit(argc, argv);
+    try
+    {
+        if(argc>1 && (strcmp(argv[1],"-s")==0))
+        {
+            StreamSockConnection con;
+            for(i=2;(int)i<argc;i+=2)
+            {
+                string addr=argv[i];
+                addr+=":5555";
+                cout << addr << endl;
+                con.connect( addr );
+            }
+            for(i=0;i<10000000;i++)
+            {
+//                cout << i << endl;
+                con.putUInt32(i);
+            }
+            con.flush();
+        }
+        if(argc>1 && (strcmp(argv[1],"-r")==0))
+        {
+            StreamSockConnection con;
+            con.bind("5555");
+            con.accept();
+            con.selectChannel();
+            for(i=0;i<10000000;i++)
+            {
+                con.getUInt32(j);
+//                cout << j << endl;
+                if(j!=i)
+                {
+                    cout << "Error: Unexpected data!!" << endl;
+                }
+            }
+        }
+    }
+    catch(exception &e)
+    {
+        cout << e.what() << endl;
+    }
+    return 0;
+}
