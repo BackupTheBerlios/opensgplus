62d4f39b8f10b09bab5e8a8de6ef3c143c48160f
jsux <jsux>
2002-04-17 17:38:05 +0000

    Initial revision

---
+++ OcclusionTest/Experimental/OcclusionTest/Experimental/OSGHPTest.h
@@ -0,0 +1,121 @@
+/*---------------------------------------------------------------------------*\
+ *                     OpenSG PLUS HPTest Culling                         *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2002 by the WSI/GRIS Uni Tuebingen              *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: staneker@gris.uni-tuebingen.de                                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef _OSGHPTEST_H_
+#define _OSGHPTEST_H_
+
+#ifdef __sgi
+#pragma once
+#endif
+
+#include <GL/gl.h>
+#include <OSGBaseTypes.h>
+#include <OSGSystemDef.h>
+#include <OSGDrawAction.h>
+#include <OSGDynamicVolume.h>
+
+OSG_BEGIN_NAMESPACE
+
+//! HPTest
+//! \ingroup RenderingBackendLib
+
+class OSG_SYSTEMLIB_DLLMAPPING HPTest
+{
+    /*==========================  PUBLIC  =================================*/
+  public:
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Statistic                                  */
+    /*! \{                                                                 */
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Constructors                               */
+    /*! \{                                                                 */
+
+    HPTest(void);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Destructor                                 */
+    /*! \{                                                                 */
+
+    virtual ~HPTest(void);
+    
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                    Access                                    */
+    /*! \{                                                                 */
+
+    void setup(const int&);
+
+    void frameInit(void);
+    void frameExit(void);
+
+    void init(void);
+    void perform(const int&, const DynamicVolume&);
+    int result(const int&);
+    void exit(void);
+    
+    /*! \}                                                                 */
+    /*=========================  PROTECTED  ===============================*/
+  protected:
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Member                                  */
+    /*! \{                                                                 */
+
+
+    /*! \}                                                                 */
+    /*==========================  PRIVATE  ================================*/
+  private:
+
+    unsigned int _maxtests;
+    GLboolean* _results;
+
+    /*! \brief prohibit default function (move to 'public' if needed) */
+    HPTest(const HPTest &source);
+    /*! \brief prohibit default function (move to 'public' if needed) */
+    void operator =(const HPTest &source);
+};
+
+OSG_END_NAMESPACE
+
+#include "OSGHPTest.inl"
+
+#endif /* _OSGHPTEST_H_ */
